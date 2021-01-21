01e05f4ecc94b334db985b499548847bec11acb9
Thilina Dilshan <ourvillage.td@gmail.com>
2021-01-21 08:19:39 +0530

    85% complete

---
+++ MySite/js/prograssbar.js
@@ -0,0 +1,25 @@
+var php = function(){
+    document.querySelector(".php").className = "php1";
+};
+var css = function(){
+    document.querySelector(".css").className = "css1";
+};
+var htm = function(){
+    document.querySelector(".htm").className = "htm1";
+};
+var java = function(){
+    document.querySelector(".java").className = "javas";
+};
+var javaFX = function(){
+    document.querySelector(".javaFX").className = "javasFX";
+};
+var jav = function(){
+    document.querySelector(".javascript").className = "javascript1";
+};
+
+setTimeout(php,1000);
+setTimeout(css,3000);
+setTimeout(htm,5000);
+setTimeout(java,7000);
+setTimeout(javaFX,9000)
+setTimeout(jav,11000);
